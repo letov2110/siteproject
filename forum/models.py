e8ee1645e7276152fb67c3045fc05b15d80de6f0
yury <letov2110@gmail.com>
2024-05-01 13:33:21 +0300

    fixed when user can't login without avatar in their profile
    fixed views avatar in site when their can't exist
    
    added rating for comment in forum
    
    added global user rating

--- forum/models.py
+++ forum/models.py
@@ -15,13 +15,20 @@ class Forum_Question(models.Model):
     cat_topic = models.ManyToManyField(Cat_topics ,blank=False)
     def __str__(self):
         return self.title
-    
-
 
 class Forum_Answer(models.Model):
     post = models.ForeignKey(Forum_Question, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True, blank=True)
-    def __str__(self): 
+    rating = models.IntegerField(default=0)
+
+    def __str__(self):
         return self.text
+    
+class VotedComment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Forum_Answer, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('user', 'answer')
