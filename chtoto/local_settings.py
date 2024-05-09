d2fb92bf61d6c2cce9fceea2e7d382e2c1279c1e
yury <letov2110@gmail.com>
2024-05-09 15:19:59 +0300

    update gitignore

--- chtoto/local_settings.py
+++
@@ -1,5 +0,0 @@
-SECRET_KEY = 'django-insecure-qt!@_dp)fegayvndo_abcyfh5=%&rt)_wv!00r&1qqb&_bef5v'
-
-ALLOWED_HOSTS = ['127.0.0.1','localost',]
-
-DEBUG = True
--- chtoto/settings_pass.py
+++
@@ -1,9 +0,0 @@
-mail_host_password = 'dmwrfidztqyhtoen'
-mail_host_user = 'buksite.test@yandex.ru'
-
-databases_password = 'yury'
-databases_name = 'mydb'
-databases_user = 'yury'
-
-key =  'django-insecure-qt!@_dp)fegad_!jcs#d*_fh5=%&rt)_wv!00r&1qqb&_bef5v'
-
